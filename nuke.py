28f7aac0010dee6618c3e7962fc3030402485bf0
DISCORDBBBB <60780527+BSGreatuser@users.noreply.github.com>
2021-12-26 15:43:01 +0900

    Update nuke.py

--- nuke.py
+++ nuke.py
@@ -6,15 +6,13 @@ client = discord.Client()
 
 @client.event
 async def on_ready():
-    print("봇이 성공적으로 실행되었습니다.")
-    game = discord.Game('★~하는중에 표시될 네임 작성★')
-    await client.change_presence(status=discord.Status.online, activity=game)
+    print(client.user)
 
 
 @client.event
 async def on_message(message):
     if message.content.startswith('!폭파'):
-        if message.author.guild_permissions.ban_members:
+        if message.author.guild_permissions.manage_messages:
             aposition = message.channel.position
             new = await message.channel.clone()
             await message.channel.delete()
@@ -23,7 +21,5 @@ async def on_message(message):
             embed = discord.Embed(title='채널 폭파됨', colour=discord.Colour.red())
             embed.set_image(url='https://media.giphy.com/media/HhTXt43pk1I1W/giphy.gif')
             await new.send(embed=embed)
-        else:
-            await message.channel.send('``명령어 사용권한이 없습니다.``')
 
 client.run('★TOKEN★')
